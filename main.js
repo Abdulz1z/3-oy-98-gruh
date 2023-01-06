4652955ff29bcb9be2b780976dbc539e98ef92e5
AbdulazizObidov <abdulazizobidov03@gmail.com>
2023-01-06 17:15:51 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var name="Ticket fee - "
+var ticket=500
+var ticketTotal=ticket*10650.34
+console.log(name+ticketTotal)
+
+var name="Hotel fee - "
+var hotel=250
+var hotelTotal=hotel*10650.34
+console.log(name+hotelTotal)
+
+var name="Museum fee - "
+var museum=120
+var museumTotal=museum*10650.03
+console.log(name+museumTotal)
+
+var name="Total - "
+var total=ticketTotal+hotelTotal+museumTotal
+console.log(name + total)
+alert (name+total)
